6640800f522210a99e6d43e0e6a6f44dfe47e5cc
unknown <syeluri@linkfields.com>
2025-10-07 16:57:24 +0530

    Added App.tsx

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+// import { useState } from 'react'
+import LoginPage from './pages/Login/login'
+function App() {
+    return (
+        <>
+            <LoginPage />
+        </>
+    )
+}
+export default App
