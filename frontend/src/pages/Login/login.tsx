989f967fcb6c3a9a4b8468bd20fa9dd0da368f1e
unknown <syeluri@linkfields.com>
2025-10-08 11:27:29 +0530

    Removed conditional classes from login and signup as there are no any conditional classes now

--- frontend/src/pages/Login/login.tsx
+++ frontend/src/pages/Login/login.tsx
@@ -1,4 +1,3 @@
-import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button"
 import { Card, CardContent } from "@/components/ui/card"
 import {
@@ -17,7 +16,7 @@ export default function LoginPage() {
     return (
         <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
             <div className="w-full max-w-sm md:max-w-4xl">
-                <div className={cn("flex flex-col gap-6")}>
+                <div className="flex flex-col gap-6">
                     <Card className="overflow-hidden p-0">
                         <CardContent className="grid p-0 md:grid-cols-2">
                             <form className="p-6 md:p-8">
